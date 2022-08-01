88ce8d0a435c036fd19d0bcc30a95ca4c19918c3
risalet <risalat411@gmail.com>
2022-08-01 18:29:41 +0900

    Primitive Data Type

---
+++ src/main/java/com/newproject/importantmethods/SubStringAndReplace.java
@@ -0,0 +1,54 @@
+package com.newproject.importantmethods;
+
+import java.util.ArrayList;
+
+public class SubStringAndReplace {
+    //SubString Another One --Return Type --Array;
+    public static void main(String[] args) {
+        String sentence = "約 205,000,00045 件 （0.54 秒）";
+        String find = "205," ;
+        ArrayList<String> findWord =new ArrayList<>();
+        for (String eachString : sentence.split(" ")){
+            if (eachString.contains(find)){
+                findWord.add(eachString);
+            }
+        }
+        System.out.println(findWord);
+    }
+}
+
+//SubString Another One --Return Type --String;
+ class Main {
+     static String findWord(String sentence, String subString) {
+         for (String word : sentence.split(" ")){
+             if (word.contains(subString)){
+                 return word;
+             }
+         }
+         return null;
+     }
+
+     public static void main(String[] args) {
+         String sentence = "約205,000,89768 件 （0.54 秒）";
+         String subString = ",";
+         String foundWord = findWord(sentence,subString);
+         foundWord =foundWord.replace("約","");
+         foundWord=foundWord.replace(",","");
+         long researchNumber =Long.parseLong(foundWord);
+         System.out.println(researchNumber);
+         if (researchNumber>1){
+             System.out.println("This test is passed");
+         }else
+             System.out.println("This test not passed");
+
+
+     }
+ }
+
+
+
+
+
+
+
+
--- src/main/java/com/newproject/javabasics/HelloWord.java
+++
@@ -1,4 +0,0 @@
-package com.newproject.javabasics;
-
-public class HelloWord {
-}
