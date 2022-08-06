6c64d9f6013386909fd02f3c709e32fd3835be09
risalet <risalat411@gmail.com>
2022-08-06 16:09:29 +0900

    Scanner

--- src/main/java/com/newproject/datatypes/ArgumentDemo.java
+++ src/main/java/com/newproject/datatypes/ArgumentDemo.java
@@ -6,6 +6,7 @@ public class ArgumentDemo {
         int i2=Integer.parseInt(args[1]);
         int i3=Integer.parseInt(args[2]);
         System.out.printf("%d+%d+%d=%d\n",i1,i2,i3,i1+i2+i3);
+
         System.out.printf("My school and course Name: " +args[4] +" "+args[5]);
 
 
---
+++ src/main/java/com/newproject/datatypes/ScannerDemo1.java
@@ -0,0 +1,14 @@
+package com.newproject.datatypes;
+
+import java.util.Scanner;
+
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Please inter you school name");
+        String school= scanner.nextLine();
+        System.out.println("Please inter you course");
+        String course =scanner.nextLine();
+        System.out.println(String .format("My school name and course  is: %s %s ",school,course));
+    }
+}
