c6006ee84395d65cefa0880d811d8989100ecd27
user <risalat411@gmail.com>
2022-08-10 17:17:18 +0900

    Css MaxNumber

--- src/main/java/com/newproject/conditionalstatement/Practice.java
+++ src/main/java/com/newproject/conditionalstatement/Practice.java
@@ -3,10 +3,38 @@ package com.newproject.conditionalstatement;
 public class Practice {
     public static void main(String[] args) {
         //Different between & && operator
-        boolean b1 =false;
-        int number =100;
-        if (b1 &&(number++>0)){
-
+       boolean  b1 = true;
+        int number = 100;
+        if (b1 && (number++ > 0)) {
+            System.out.println("True statement");
+        } else {
+            System.out.println("False statement");
         }
+        System.out.println("Value of number:" + number);
+
+        int number1 = 100;
+        if (b1 & (number1++) > 0) {
+            System.out.println("True statement");
+        } else {
+            System.out.println("False statement");
+        }
+        System.out.println("Value of number:" + number1);
+
+        int number2=100;
+        if ((number2++) > 0 & b1) {
+            System.out.println("True statement");
+        } else {
+            System.out.println("False statement");
+        }
+        System.out.println("Value of number:" + number1);
+
+        int number3=100;
+        if ((number++ > 0)&& b1 ) {
+            System.out.println("True statement");
+        } else {
+            System.out.println("False statement");
+        }
+        System.out.println("Value of number:" + number);
     }
+
 }
---
+++ src/main/java/com/newproject/interview/SwitchCase.java
@@ -0,0 +1,28 @@
+package com.newproject.interview;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int number =1234;
+        int digitCounts= String.valueOf(number).length();
+        switch (digitCounts){
+            case 1:
+                System.out.println("This number is less than 9");
+                break;
+            case 2:
+                System.out.println("This number is between 10 and 99 ");
+                break;
+            case 3:
+                System.out.println("This number is between 100 and 999 ");
+                break;
+            case 4:
+                System.out.println("This number is between 1000 and 9999 ");
+                break;
+            default:
+                System.out.println("THis is a number");
+        }
+
+    }
+
+}
